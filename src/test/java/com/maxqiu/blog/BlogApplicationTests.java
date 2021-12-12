b096ab40f67d98d00d7d9e38a22f025ca17f21cb
Max-Qiu <qzhing@126.com>
2021-12-13 07:30:12 +0800

    项目初始化

---
+++ src/test/java/com/maxqiu/blog/BlogApplicationTests.java
@@ -0,0 +1,10 @@
+package com.maxqiu.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+    @Test
+    void contextLoads() {}
+}

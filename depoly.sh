3b71a9e77daddc7bd39c9ae2c81bc603d86ee116
Max-Qiu <qzhing@126.com>
2022-05-02 16:24:02 +0800

    1. 升级部分依赖 2. 完善部署脚本

--- depoly.sh
+++ depoly.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cd /work/project/min-blog/
+cd /work/project/min-blog/ || exit
 git pull
 mvn clean package
 VERSION=$(date "+%Y%m%d")
-docker build -t min-blog:$VERSION .
+docker build -t min-blog:"$VERSION" .
 docker rm -f min-blog
-docker run -d --restart always --name min-blog -v /work/log/min-blog:/log -v /work/min-blog-files/:/min-blog-files --network min-blog-bridge min-blog:$VERSION
+docker run -d --restart always --name min-blog -v /work/log/min-blog:/log -v /work/min-blog-files/:/min-blog-files --network min-blog-bridge min-blog:"$VERSION"
 docker image prune -f
 docker restart nginx

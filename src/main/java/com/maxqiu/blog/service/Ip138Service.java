5b6303cfe992db87183ecff3449752be77ac402e
Max-Qiu <qzhing@126.com>
2024-03-12 23:08:25 +0800

    1. 修改 ip138 请求地址 2. 更新历史说明

--- src/main/java/com/maxqiu/blog/service/Ip138Service.java
+++ src/main/java/com/maxqiu/blog/service/Ip138Service.java
@@ -39,7 +39,7 @@ public class Ip138Service {
         if (!ip138Properties.getEnable()) {
             return null;
         }
-        String urlString = "https://api.ip138.com/ip/?ip=" + strIp + "&datatype=jsonp";
+        String urlString = "https://api.ipshudi.com/ip/?ip=" + strIp + "&datatype=jsonp";
         try {
             URL url = new URL(urlString);
             HttpURLConnection conn = (HttpURLConnection)url.openConnection();
